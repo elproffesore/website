feadaf1ceb7de158648e5cefa0156a6b2d767370
elproffesore <philipp.proff@gmx.de>
2025-12-05 16:41:19 +0100

    feat: set base path and smaller padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,9 @@
     <meta property="og:url" content="https://philippproff.eu/">
 	<title>Portfolio | Philipp Proff</title>
 </svelte:head>
-<div class="bg-background text-text min-h-screen min-w-screen p-8 grid grid-cols-12">
+<div class="bg-background text-text min-h-screen min-w-screen p-2 grid grid-cols-12">
 	<h1 class="col-start-2 col-span-12 md:col-start-3 md:col-span-5 mb-8">
-		<a href={LinkHandler("/")}>Projects</a> / <a href={LinkHandler("/info")}>Info</a>
+		<a href={LinkHandler("")}>Projects</a> / <a href={LinkHandler("/info")}>Info</a>
 	</h1>
 	{@render children()}
 </div>
